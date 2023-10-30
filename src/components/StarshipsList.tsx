397fdb2d952590de58cc9073b920ff0c48ad1a76
SergioGarciaF <sergiogarciafernandez4@gmail.com>
2023-10-30 11:01:07 +0100

    Exercici-3 correción

--- src/components/StarshipsList.tsx
+++ src/components/StarshipsList.tsx
@@ -13,9 +13,10 @@ const StarshipsList: React.FC = () => {
   const { list: starships, next } = useSelector((state: RootState) => state.starships);
   const dispatch = useDispatch();
 
-  
-  
 
+  useEffect(() => {
+    dispatch(fetchStarships())
+  }, [])
 
   const fetchMoreStarships = () => {
     if (next) {
@@ -23,27 +24,27 @@ const StarshipsList: React.FC = () => {
     }
   }
 
-  return ( 
+  return (
 
-      <InfiniteScroll dataLength={starships.length}
-        next={fetchMoreStarships}
-        hasMore={!!next}
-        loader={<span className="loading loading-ring loading-lg text-accent"></span>}>
-        <ul>
-          {starships.map((el: Starship, index: number) => (
-            <li key={index}>
-              <Link to={`/starships/${el.name}`}>
-                <div className="card w-96 bg-success text-neutral-content mt-2">
-                  <div className="card-body items-center text-center">
-                    <h2 className="text-accent">{el.name}</h2>
-                    <p className="text-accent">{el.model}</p>
-                  </div>
+    <InfiniteScroll dataLength={starships.length}
+      next={fetchMoreStarships}
+      hasMore={!!next}
+      loader={<span className="loading loading-ring loading-lg text-accent"></span>}>
+      <ul>
+        {starships.map((el: Starship, index: number) => (
+          <li key={index}>
+            <Link to={`/starships/${el.name}`}>
+              <div className="card w-96 bg-success text-neutral-content mt-2 mx-auto">
+                <div className="card-body items-center text-center">
+                  <h2 className="text-accent">{el.name}</h2>
+                  <p className="text-accent">{el.model}</p>
                 </div>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </InfiniteScroll>
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </InfiniteScroll>
   );
 }
 
